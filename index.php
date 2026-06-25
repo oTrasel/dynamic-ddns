7807ec457774ed692f0ed07465ece61f02d5deeb
Lucas Gonçalves <71295470+oTrasel@users.noreply.github.com>
2026-06-24 23:12:52 -0300

    Add timestamps to IP verification messages

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $dotenv->load();
 $verificaIp = new VerifyIp();
 $verificaIp->verificaIp();
 $isNovoIp = $verificaIp->isNovoIp();
-echo 'INICIANDO VERIFICACAO'. PHP_EOL;
+echo '[' . date('d/m/Y H:i:s') . '] INICIANDO VERIFICACAO' . PHP_EOL;
 if ($isNovoIp) {
     $apiKey = $_ENV['API_KEY'];
 
@@ -25,10 +25,10 @@ if ($isNovoIp) {
     $novoIp = $verificaIp->getIpv4Address();
     $updateDns->setIpv4Address($novoIp);
     $updateDns->update();
-    echo 'IP ATUALIZADO' . PHP_EOL;
+    echo '[' . date('d/m/Y H:i:s') . '] IP ATUALIZADO' . PHP_EOL;
     exit;
     
 }else{
-    echo 'SEM ALTERACAO NO IP'. PHP_EOL;
+    echo '[' . date('d/m/Y H:i:s') . '] SEM ALTERACAO NO IP'. PHP_EOL;
     exit;
 }
